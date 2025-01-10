cdaff4bc40869d2fcb318200739be3cf1fd10548
Muhammad Hibban Syakir <144977923+Muhisya@users.noreply.github.com>
2025-01-10 08:38:30 +0700

    gf-tjs

---
+++ Meeting-5/style.js
@@ -0,0 +1,16 @@
+// soal 1
+let dressSize = 45;
+console.log(typeof dressSize);
+
+// jawaban 1
+// Mengubah tipedata number menjadi string
+dressSize = String(dressSize);
+console.log(typeof dressSize);
+
+// soal 2
+let isLight = true;
+console.log(typeof isLight);
+
+// Jawaban 1
+isLight = String(isLight);
+console.log(isLight);
