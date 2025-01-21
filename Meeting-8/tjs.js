d12a93d383d8f421e6a92af7687f5da7c91142fc
Muhammad Hibban Syakir <144977923+Muhisya@users.noreply.github.com>
2025-01-21 13:36:51 +0700

    tugas#9

---
+++ Meeting-8/tjs.js
@@ -0,0 +1,27 @@
+// Pertanyaan 1
+let nilaiAndiMtk = 91;
+let nilaiAndiIpa = 81;
+let nilaiAndiIps = 91;
+
+if (nilaiAndiMtk >= 91 && nilaiAndiIpa >= 91 && nilaiAndiIps >= 91) {
+    console.log('Anda Lulus');
+} else {
+    console.log('Tidak Lulus');
+}
+
+// Pertanyaan 2
+
+if (nilaiAndiMtk >= 91 || nilaiAndiIpa >= 91 || nilaiAndiIps || 91) {
+    console.log('Anda Lulus');
+} else {
+    console.log('Tidak Lulus');
+}
+
+// Pertanyaan 3
+let isLoggedIn = true;
+
+if (isLoggedIn) {
+    console.log('anda sudah login');
+} else {
+    console.log('anda belum login');
+}
