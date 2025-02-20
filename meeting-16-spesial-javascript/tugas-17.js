3234cff5c7bc4e8fae0fb210b8fee4c634b34230
Muhammad Hibban Syakir <144977923+Muhisya@users.noreply.github.com>
2025-02-20 11:59:39 +0700

    tugas 17

---
+++ meeting-16-spesial-javascript/tugas-17.js
@@ -0,0 +1,27 @@
+const prompt = require('prompt');
+
+// ! 1. Soal 1
+let umur = 25; 
+console.log(umur);
+console.log(umur);  
+
+// ! 2. Soal 2
+x = 10;
+console.log(x); // x is not defined
+
+// ! 3. Soal 3
+let data = "Halo";
+console.log(typeof data);
+
+data = 42;
+console.log(typeof data);
+
+data = true;
+console.log(typeof data);
+
+// ! 4. Soal 4
+// let name = prompt("What is your name?");
+// alert(`"Hello  ${name}, Selamat datang di Pondok"`);
+
+// ! 5. Soal 5
+
