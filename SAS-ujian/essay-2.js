68a682bc49adf48d4bd161d12db1ee114c2fc71b
Muhammad Hibban Syakir <144977923+Muhisya@users.noreply.github.com>
2025-03-06 10:01:36 +0700

    Sas ujian praktek dan essay

---
+++ SAS-ujian/essay-2.js
@@ -0,0 +1,10 @@
+// Membuat Sebuah website memiliki sistem login sederhana dengan aturan
+
+let login = prompt("Masukkan Username");
+let password = prompt("Masukkan Password");
+
+if (login == "admin" && password == "12345") {
+    alert("Login Berhasil");
+} else {
+    alert("Login Gagal");
+}
---
+++ SAS-ujian/essay-3.js
@@ -0,0 +1,12 @@
+// Membuat Sebuah website memiliki sistem login sederhana dengan aturan
+
+function login (username, password) {
+    if (username == "admin" && password == "12345") {
+        alert("Login Berhasil");
+    } else {
+        alert("Login Gagal");
+    }
+}
+
+let x;
+console.log(x ?? "Default Value");  
